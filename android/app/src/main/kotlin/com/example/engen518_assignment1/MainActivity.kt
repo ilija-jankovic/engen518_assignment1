970de0e07408b2c758c6769f6f159223bdc106b4
ilija-jankovic <58715208+ilija-jankovic@users.noreply.github.com>
2022-04-13 22:36:55 +1200

    Added bcrypt support for iOS and Android

--- android/app/src/main/kotlin/com/example/engen518_assignment1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/engen518_assignment1/MainActivity.kt
@@ -2,5 +2,5 @@ package com.example.engen518_assignment1
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:engen518_assignment1/auth.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await initAuth();
   runApp(const MaterialApp(home: HomePage()));
 }
