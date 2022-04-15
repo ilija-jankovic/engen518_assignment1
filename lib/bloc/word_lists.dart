6f92f36f5cd48ac18469fc561176723662437b49
ilija-jankovic <58715208+ilija-jankovic@users.noreply.github.com>
2022-04-15 16:17:30 +1200

    Documented word_lists.dart

--- lib/bloc/word_lists.dart
+++ lib/bloc/word_lists.dart
@@ -1,15 +1,21 @@
-import 'dart:convert';
-import 'dart:io';
-
-import 'package:flutter/services.dart';
-
 /// Profanity JSON list from: https://github.com/zacanger/profane-words
 /// Includes number substitutions for letters.
 ///
 /// Common password list from: https://github.com/danielmiessler/SecLists/tree/master/Passwords/Common-Credentials
 
-late List<String> _profanityList, _commonPasswords;
+import 'dart:convert';
+import 'package:flutter/services.dart';
 
+/// [List] containing profane words, including number substitutions for
+/// similarly shaped letters.
+late List<String> _profanityList;
+
+/// [List] containing top ten million common passwords.
+late List<String> _commonPasswords;
+
+/// Retrieves and captures data for [_profanityList] and [_commonPasswords].
+///
+/// Relevant files are found under /assets/.
 Future<void> initWordLists() async {
   var data = await rootBundle.loadString('assets/profanity_list.json');
   _profanityList = (jsonDecode(data) as List).cast<String>();
@@ -18,6 +24,7 @@ Future<void> initWordLists() async {
   _commonPasswords = data.split('\n');
 }
 
+/// Returns first profane word found within [text].
 String? getProfanity(String text) {
   text = text.toLowerCase();
   for (final word in _profanityList) {
@@ -28,6 +35,7 @@ String? getProfanity(String text) {
   return null;
 }
 
+/// Checks if [password] is within [_commonPasswords].
 bool isCommon(String password) {
   return _commonPasswords.contains(password);
 }
