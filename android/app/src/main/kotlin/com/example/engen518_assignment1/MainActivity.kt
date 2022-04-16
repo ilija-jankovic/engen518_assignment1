a7a29bf11973ff5e6a2d09e32dc3581b0fbcfadb
ilija-jankovic <58715208+ilija-jankovic@users.noreply.github.com>
2022-04-16 14:42:06 +1200

    Fixed APK build error

--- android/app/src/main/kotlin/com/example/engen518_assignment1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/engen518_assignment1/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.engen518_assignment1
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity: FlutterFragmentActivity() {
 }
